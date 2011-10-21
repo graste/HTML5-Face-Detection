dfd5d5405c94da22e3e5670c0f4d19def669e86b
unknown <graste@graste-thinkpad.(none)>
2011-10-21 21:37:54 +0200

    quick and dirty test to get camera support working via getUserMedia support - works in Opera Labs Camera build - try play/pause button :-)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,12 +6,30 @@ var		video = document.querySelector('video'),
 		ctx = canvas.getContext("2d");
 		glasses.src = "i/glasses.png";
 
+if (navigator.getUserMedia) {
+    navigator.getUserMedia('video', successCallback, errorCallback);
+    function successCallback(stream) {
+        alert('yeah! camera support!');
+        video.src = stream;
+        vidInterval = setInterval(html5glasses, 200);
+    }
+    function errorCallback(error) {
+        alert('An error occurred while trying to get camera access: ' + error.code);
+	return;
+    }
+}
+
 function html5glasses() {
 	// Start the clock 
 	var elapsed_time = (new Date()).getTime();
-
+	
 	// Draw the video to canvas
 	ctx.drawImage(video, 0, 0, video.width, video.height, 0, 0, canvas.width, canvas.height);
+
+        if (video.paused) {
+	    time_dump.innerHTML = "no processing as video is paused. " + ((new Date()).getTime() - elapsed_time).toString() + "ms";
+            return;
+        }
 
 	// use the face detection library to find the face
 	var comp = ccv.detect_objects({ "canvas" : (ccv.pre(canvas)),
@@ -29,7 +47,7 @@ function html5glasses() {
 }
 
 /* Events */ 
-
+/*
 video.addEventListener('play', function() {
 	vidInterval = setInterval(html5glasses,200); 
 });
@@ -38,3 +56,4 @@ video.addEventListener('ended', function() {
 	clearInterval(vidInterval);
 	time_dump.innerHTML = "finished";
 });
+*/
